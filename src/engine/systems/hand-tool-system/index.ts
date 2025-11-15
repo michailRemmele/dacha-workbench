a31ef4401ce38c4860edd9067e7f8877b46f77cd
Mikhail Remmele <mikhail.remmele@gmail.com>
2025-11-15 11:21:39 +0300

    add camera zoom and movement on wheel event

--- src/engine/systems/hand-tool-system/index.ts
+++ src/engine/systems/hand-tool-system/index.ts
@@ -1,95 +1,128 @@
-import {
-  SceneSystem,
-  Transform,
-  Camera,
-  Vector2,
-} from 'dacha'
-import type {
-  World,
-  SceneSystemOptions,
-  Actor,
-} from 'dacha'
-import type { MouseControlEvent } from 'dacha/events'
+import { SceneSystem, Transform, Camera, Vector2 } from 'dacha';
+import type { World, SceneSystemOptions, Actor } from 'dacha';
+import type { MouseControlEvent } from 'dacha/events';
 
-import { EventType } from '../../../events'
-import { persistentStorage } from '../../../persistent-storage'
+import { CANVAS_ROOT } from '../../../consts/root-nodes';
+import { EventType } from '../../../events';
+import { persistentStorage } from '../../../persistent-storage';
 
-const DEFAULT_POS_X = 0
-const DEFAULT_POS_Y = 0
+const DEFAULT_POS_X = 0;
+const DEFAULT_POS_Y = 0;
+const WHEEL_MOVE_FACTOR = 0.5;
 
 export class HandToolSystem extends SceneSystem {
-  private world: World
-  private mainActor: Actor
+  private world: World;
+  private mainActor: Actor;
+  private rootNode: HTMLElement;
 
-  private isMoving: boolean
-  private anchor: Vector2
+  private isCursorTracking: boolean;
+  private anchor: Vector2;
 
   constructor(options: SceneSystemOptions) {
-    super()
+    super();
 
-    const { world } = options
+    const { world } = options;
 
-    this.world = world
-    this.mainActor = world.data.mainActor as Actor
+    this.world = world;
+    this.mainActor = world.data.mainActor as Actor;
 
-    this.isMoving = false
-    this.anchor = new Vector2(0, 0)
+    this.rootNode = document.getElementById(CANVAS_ROOT) as HTMLElement;
 
-    this.world.addEventListener(EventType.SelectScene, this.handleSceneChange)
-    this.world.addEventListener(EventType.CameraMoveStart, this.handleCameraMoveStart)
-    this.world.addEventListener(EventType.CameraMoveEnd, this.handleCameraMoveEnd)
-    this.world.addEventListener(EventType.CameraMove, this.handleCameraMove)
+    this.isCursorTracking = false;
+    this.anchor = new Vector2(0, 0);
+
+    this.world.addEventListener(EventType.SelectScene, this.handleSceneChange);
+    this.world.addEventListener(
+      EventType.CameraMoveStart,
+      this.handleCameraMoveStart,
+    );
+    this.world.addEventListener(
+      EventType.CameraMoveEnd,
+      this.handleCameraMoveEnd,
+    );
+    this.world.addEventListener(EventType.CameraMove, this.handleCameraMove);
+
+    this.rootNode.addEventListener('wheel', this.handleWheel);
   }
 
   onSceneDestroy(): void {
-    this.world.removeEventListener(EventType.SelectScene, this.handleSceneChange)
-    this.world.removeEventListener(EventType.CameraMoveStart, this.handleCameraMoveStart)
-    this.world.removeEventListener(EventType.CameraMoveEnd, this.handleCameraMoveEnd)
-    this.world.removeEventListener(EventType.CameraMove, this.handleCameraMove)
+    this.world.removeEventListener(
+      EventType.SelectScene,
+      this.handleSceneChange,
+    );
+    this.world.removeEventListener(
+      EventType.CameraMoveStart,
+      this.handleCameraMoveStart,
+    );
+    this.world.removeEventListener(
+      EventType.CameraMoveEnd,
+      this.handleCameraMoveEnd,
+    );
+    this.world.removeEventListener(EventType.CameraMove, this.handleCameraMove);
+
+    this.rootNode.removeEventListener('wheel', this.handleWheel);
   }
 
   private handleSceneChange = (): void => {
-    const transform = this.mainActor.getComponent(Transform)
-    transform.offsetX = DEFAULT_POS_X
-    transform.offsetY = DEFAULT_POS_Y
+    const transform = this.mainActor.getComponent(Transform);
+    transform.offsetX = DEFAULT_POS_X;
+    transform.offsetY = DEFAULT_POS_Y;
 
-    persistentStorage.set('canvas.mainActor.transform.offsetX', transform.offsetX)
-    persistentStorage.set('canvas.mainActor.transform.offsetY', transform.offsetY)
-  }
+    this.saveCameraPosition(transform.offsetX, transform.offsetY);
+  };
 
   private handleCameraMoveStart = (event: MouseControlEvent): void => {
-    const { screenX, screenY } = event
+    const { screenX, screenY } = event;
 
-    this.isMoving = true
-    this.anchor.x = screenX
-    this.anchor.y = screenY
-  }
+    this.isCursorTracking = true;
+    this.anchor.x = screenX;
+    this.anchor.y = screenY;
+  };
 
   private handleCameraMoveEnd = (): void => {
-    const transform = this.mainActor.getComponent(Transform)
-
-    persistentStorage.set('canvas.mainActor.transform.offsetX', transform.offsetX)
-    persistentStorage.set('canvas.mainActor.transform.offsetY', transform.offsetY)
-
-    this.isMoving = false
-  }
+    this.isCursorTracking = false;
+  };
 
   private handleCameraMove = (event: MouseControlEvent): void => {
-    if (!this.isMoving) {
-      return
+    if (!this.isCursorTracking) {
+      return;
     }
 
-    const { screenX, screenY } = event
+    const { screenX, screenY } = event;
 
-    const transform = this.mainActor.getComponent(Transform)
-    const { zoom } = this.mainActor.getComponent(Camera)
+    const transform = this.mainActor.getComponent(Transform);
+    const { zoom } = this.mainActor.getComponent(Camera);
 
-    transform.offsetX += (this.anchor.x - screenX) / zoom
-    transform.offsetY += (this.anchor.y - screenY) / zoom
+    transform.offsetX += (this.anchor.x - screenX) / zoom;
+    transform.offsetY += (this.anchor.y - screenY) / zoom;
 
-    this.anchor.x = screenX
-    this.anchor.y = screenY
+    this.anchor.x = screenX;
+    this.anchor.y = screenY;
+
+    this.saveCameraPosition(transform.offsetX, transform.offsetY);
+  };
+
+  private handleWheel = (event: WheelEvent): void => {
+    if (event.ctrlKey) {
+      return;
+    }
+
+    const transform = this.mainActor.getComponent(Transform);
+    const { zoom } = this.mainActor.getComponent(Camera);
+
+    const deltaX = (event.deltaX * WHEEL_MOVE_FACTOR) / zoom;
+    const deltaY = (event.deltaY * WHEEL_MOVE_FACTOR) / zoom;
+
+    transform.offsetX += deltaX;
+    transform.offsetY += deltaY;
+
+    this.saveCameraPosition(transform.offsetX, transform.offsetY);
+  };
+
+  private saveCameraPosition(x: number, y: number): void {
+    persistentStorage.set('canvas.mainActor.transform.offsetX', x);
+    persistentStorage.set('canvas.mainActor.transform.offsetY', y);
   }
 }
 
-HandToolSystem.systemName = 'HandToolSystem'
+HandToolSystem.systemName = 'HandToolSystem';
---
+++ src/engine/systems/zoom-tool-system/consts.ts
@@ -0,0 +1,5 @@
+export const ZOOM_FACTOR = 1.5;
+export const WHEEL_ZOOM_STEP = 0.05;
+export const MIN_ZOOM = 0.01;
+export const MAX_ZOOM = 100;
+export const DEFAULT_ZOOM = 1;

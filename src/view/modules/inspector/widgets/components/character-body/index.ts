15f5dc59b988a50e3e3db4ed86bfa55978228f1f
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-06-16 23:16:22 +0300

    update character body widget fields

--- src/view/modules/inspector/widgets/components/character-body/index.ts
+++ src/view/modules/inspector/widgets/components/character-body/index.ts
@@ -3,12 +3,25 @@ import type { WidgetSchema } from '../../../../../../types/widget-schema';
 export const characterBody: WidgetSchema = {
   fields: [
     {
+      name: 'motionMode',
+      type: 'select',
+      options: ['surface', 'free'],
+    },
+    {
       name: 'upDirectionX',
       type: 'number',
+      dependency: {
+        name: 'motionMode',
+        value: 'surface',
+      },
     },
     {
       name: 'upDirectionY',
       type: 'number',
+      dependency: {
+        name: 'motionMode',
+        value: 'surface',
+      },
     },
     {
       name: 'skinWidth',
@@ -17,14 +30,26 @@ export const characterBody: WidgetSchema = {
     {
       name: 'maxSlopeAngle',
       type: 'number',
+      dependency: {
+        name: 'motionMode',
+        value: 'surface',
+      },
     },
     {
       name: 'maxSlides',
       type: 'number',
     },
     {
-      name: 'groundProbeDistance',
+      name: 'maxRecoveries',
       type: 'number',
+    },
+    {
+      name: 'groundSnapDistance',
+      type: 'number',
+      dependency: {
+        name: 'motionMode',
+        value: 'surface',
+      },
     },
     {
       name: 'disabled',
@@ -32,12 +57,14 @@ export const characterBody: WidgetSchema = {
     },
   ],
   getInitialState: () => ({
+    motionMode: 'surface',
     upDirectionX: 0,
     upDirectionY: -1,
     skinWidth: 0.1,
     maxSlopeAngle: 45,
     maxSlides: 4,
-    groundProbeDistance: 1,
+    maxRecoveries: 3,
+    groundSnapDistance: 1,
     disabled: false,
   }),
 };

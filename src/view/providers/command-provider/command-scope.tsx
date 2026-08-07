2a54758815991c23a5de7faa37bfd3cf1567f052
Mikhail Remmele <mikhail.remmele@gmail.com>
2026-08-07 22:16:29 +0300

    fix method call

--- src/view/providers/command-provider/command-scope.tsx
+++ src/view/providers/command-provider/command-scope.tsx
@@ -1,41 +1,40 @@
-import React, {
-  useEffect,
-  useContext,
-  FC,
-} from 'react'
+import React, { useEffect, useContext, FC } from 'react';
 
-import { ROOT_SCOPE } from '../../../consts/scopes'
+import { ROOT_SCOPE } from '../../../consts/scopes';
 
-import { CommandContext } from './command-provider'
+import { CommandContext } from './command-provider';
 
 interface CommandScopeProps {
-  name?: string
-  children: JSX.Element | JSX.Element[]
+  name?: string;
+  children: JSX.Element | JSX.Element[];
 }
 
-export const CommandScopeContext = React.createContext<string>(ROOT_SCOPE)
+export const CommandScopeContext = React.createContext<string>(ROOT_SCOPE);
 
 export const CommandScopeProvider: FC<CommandScopeProps> = ({
   name = ROOT_SCOPE,
   children,
 }): JSX.Element => {
-  const { store, setActiveScope } = useContext(CommandContext)
+  const { store, setActiveScope } = useContext(CommandContext);
 
   useEffect(() => {
-    setActiveScope(name)
+    setActiveScope(name);
 
     return (): void => {
-      setActiveScope(ROOT_SCOPE)
-    }
-  }, [name])
+      setActiveScope(ROOT_SCOPE);
+    };
+  }, [name]);
 
-  useEffect(() => (): void => {
-    store.clean({ scope: name })
-  }, [])
+  useEffect(
+    () => (): void => {
+      store.clear({ scope: name });
+    },
+    [],
+  );
 
   return (
     <CommandScopeContext.Provider value={name}>
       {children}
     </CommandScopeContext.Provider>
-  )
-}
+  );
+};
